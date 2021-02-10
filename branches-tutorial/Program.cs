3124cef6a9c966020e79da47883d6e8cc8d801ad
Denzyl Fontaine <ddenzfong@gmail.com>
2021-02-10 01:34:36 -0600

    finished tutorial on loops and lists

--- branches-tutorial/Program.cs
+++ branches-tutorial/Program.cs
@@ -4,17 +4,18 @@ namespace BranchesAndLoops
 {
     class Program
     {
-        static void Main(string[] args)
+        static void ExploreIf(string[] args)
         {
-            int a = 5;
-            int b= 3;
-            if(a +b > 10)
+            int a = 7;
+            int b = 4;
+            if(a + b > 10)
             Console.WriteLine("greater than 10");
             else{
                 Console.WriteLine("not greater than 10");
             }
 
-            int c = 4;
+            int c = 1;
+            //both must be true for the argument to work this is because && was used 
             if((a + b + c > 10) && (a == b))
             {
                 Console.WriteLine("greater than 10");
@@ -24,6 +25,72 @@ namespace BranchesAndLoops
             {
                 Console.WriteLine("not greater than 10");
                 Console.WriteLine("and the first number is not equal to the second");
+            }
+
+            // this is an or either option can be correct
+            if((a +b + c > 10) || (a == b))
+            {
+                Console.WriteLine("the answer is greater than 10");
+                Console.WriteLine("or the first and second numbers are equal");
+            } else 
+            {
+                Console.WriteLine("the answer is not greater than 10");
+                Console.WriteLine("and the first number is not equal to the second");
+            }
+        }
+
+        static void ExploreWhile(string[] args)
+        {
+
+            //ExploreIf();
+
+            int counter = 13;
+            while (counter < 10)
+            {
+                Console.WriteLine($"the practice counter is {counter}");
+                counter++;
+            }
+
+            int doCounter = 0;
+            do
+            {
+                Console.WriteLine($"The do counter is {doCounter}");
+                doCounter++;
+            } while (doCounter < 10);
+        }
+
+        static void Main(string[] args)
+        {
+
+            //ExploreWhile();
+            /*
+            for (int index = 7; index <= 10; index++)
+            {
+                Console.WriteLine($"The index is {index}");
+            }
+
+            for (int column = 'a'; column < 'k'; column++)
+            {
+                Console.WriteLine($"the column is {column}");
+                for (int row = 1; row < 11; row++)
+                {
+                   Console.WriteLine($"the row is {row}"); 
+                }
+            }
+            */
+
+            //for every number check if it divides by 3 evenly then count up and check again
+            //only display if true
+            for (int counter = 1; counter < 20;)
+            {
+                if((counter % 3) == 0)
+                {
+                    Console.WriteLine($"{counter} is divisble by 3");
+                    counter++;
+                }else
+                {
+                    counter++;
+                }
             }
         }
     }
---
+++ list-tutorial/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WorkingWithStrings();   
+
+            var fibonacciNumbers = new List<int> {1, 1};
+
+            var previous = fibonacciNumbers[fibonacciNumbers.Count -1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count -2];
+
+            
+            
+            for (int count = 0; count < 20;)
+            {
+                fibonacciNumbers.Add(previous + previous2);
+                Console.WriteLine($"{fibonacciNumbers[0]}");
+                count++;      
+
+                /* foreach (var item in fibonacciNumbers)
+                {
+                    Console.WriteLine(item);
+                } */
+            }
+        }
+        
+        static void WorkingWithStrings(string[] args)
+        {
+            //create a list and add 3 names to it 
+            var names = new List<string> { "Denzyl", "Ana", "Felipe"};
+            Console.WriteLine();
+            //add 2 names and remove one
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            Console.WriteLine($"my name is {names[0]}");
+            Console.WriteLine($"i've added {names[2]} and {names[3]}");
+            Console.WriteLine($"the list has {names.Count} people in it");
+
+            var index = names.IndexOf("Felipe");
+            Console.WriteLine($"index is {names[index]}");
+            if(index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf return {index}");
+            }
+            else
+            {
+                Console.WriteLine($"the name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"when an item is not found, indexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"the name {names[index]} is at index {index}");
+            }
+
+            //make list alphabetical 
+            names.Sort();
+            //print a string for every name in the list
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
